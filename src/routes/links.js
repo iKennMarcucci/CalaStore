3ee744331b707513d4104c08135f9f14b32399a3
Kenn Marcucci <marcucci5783@gmail.com>
2022-08-23 23:38:45 -0500

    Carrito Terminado

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,22 +1,97 @@
 const express = require('express');
 const router = express.Router();
-
 const pool = require('../database');
+const arrayProductos = [];
 
-router.get('/index', async (req, res) =>{
-    const bodys = await pool.query("SELECT nombre, FORMAT(p.precio, 'C', 'en-us') as precio, img FROM productos p WHERE id_categoria = 1");
-    const croptops = await pool.query("SELECT nombre, FORMAT(p.precio, 'C', 'en-us') as precio, img FROM productos p WHERE id_categoria = 2");
-    const leggingsSM = await pool.query("SELECT nombre, FORMAT(p.precio, 'C', 'en-us') as precio, img FROM productos p WHERE id_categoria = 3");
-    const leggingsLXL = await pool.query("SELECT nombre, FORMAT(p.precio, 'C', 'en-us') as precio, img FROM productos p WHERE id_categoria = 4");
-    const tops = await pool.query("SELECT nombre, FORMAT(p.precio, 'C', 'en-us') as precio, img FROM productos p WHERE id_categoria = 5");
-    const conjuntos = await pool.query("SELECT nombre, FORMAT(p.precio, 'C', 'en-us') as precio, img FROM productos p WHERE id_categoria = 6");
-    const blusas = await pool.query("SELECT nombre, FORMAT(p.precio, 'C', 'en-us') as precio, img FROM productos p WHERE id_categoria = 7");
-    const enterizos = await pool.query("SELECT nombre, FORMAT(p.precio, 'C', 'en-us') as precio, img FROM productos p WHERE id_categoria = 8");
+router.get('/index', async (req, res) => {
+    const bodys = await pool.query("SELECT id, id_categoria, nombre, precio, img FROM productos p WHERE id_categoria = 1");
+    const croptops = await pool.query("SELECT id, id_categoria, nombre, precio, img FROM productos p WHERE id_categoria = 2");
+    const leggingsSM = await pool.query("SELECT id, id_categoria, nombre, precio, img FROM productos p WHERE id_categoria = 3");
+    const leggingsLXL = await pool.query("SELECT id, id_categoria, nombre, precio, img FROM productos p WHERE id_categoria = 4");
+    const tops = await pool.query("SELECT id, id_categoria, nombre, precio, img FROM productos p WHERE id_categoria = 5");
+    const conjuntos = await pool.query("SELECT id, id_categoria, nombre, precio, img FROM productos p WHERE id_categoria = 6");
+    const blusas = await pool.query("SELECT id, id_categoria, nombre, precio, img FROM productos p WHERE id_categoria = 7");
+    const enterizos = await pool.query("SELECT id, id_categoria, nombre, precio, img FROM productos p WHERE id_categoria = 8");
+
     res.render('links/index', { bodys, croptops, leggingsSM, leggingsLXL, tops, conjuntos, blusas, enterizos });
 });
 
-router.get('/cart', (req, res) =>{
-    res.render('links/cart');
+router.post('/index', async (req, res) => {
+    const { id, id_categoria, nombre, precio, img } = req.body;
+
+    const newProducto = {
+        id,
+        id_categoria,
+        nombre,
+        precio,
+        img,
+        linkimg: "none"
+    };
+    arrayProductos.push(newProducto);
+    res.redirect('/links/index');
+});
+
+router.get('/cart', async (req, res) => {
+    var bool = false;
+    var total = 0;
+    if (arrayProductos.length >= 1) {
+        bool = true;
+        for (let i = 0; i < arrayProductos.length; i++) {
+            if (arrayProductos[i].id_categoria == 1) {
+                const nombreimg = arrayProductos[i].img;
+                if (arrayProductos[i].linkimg == "none") {
+                    arrayProductos[i].linkimg = "/img/productos/Bodys/" + nombreimg;
+                }
+            }
+            if (arrayProductos[i].id_categoria == 2) {
+                const nombreimg = arrayProductos[i].img;
+                if (arrayProductos[i].linkimg == "none") {
+                    arrayProductos[i].linkimg = "/img/productos/CropTops/" + nombreimg;
+                }
+            }
+            if (arrayProductos[i].id_categoria == 3) {
+                const nombreimg = arrayProductos[i].img;
+                if (arrayProductos[i].linkimg == "none") {
+                    arrayProductos[i].linkimg = "/img/productos/LeggingsSM/" + nombreimg;
+                }
+            }
+            if (arrayProductos[i].id_categoria == 4) {
+                const nombreimg = arrayProductos[i].img;
+                if (arrayProductos[i].linkimg == "none") {
+                    arrayProductos[i].linkimg = "/img/productos/LeggingsLXL/" + nombreimg;
+                }
+            }
+            if (arrayProductos[i].id_categoria == 5) {
+                const nombreimg = arrayProductos[i].img;
+                if (arrayProductos[i].linkimg == "none") {
+                    arrayProductos[i].linkimg = "/img/productos/Tops/" + nombreimg;
+                }
+            }
+            if (arrayProductos[i].id_categoria == 6) {
+                const nombreimg = arrayProductos[i].img;
+                if (arrayProductos[i].linkimg == "none") {
+                    arrayProductos[i].linkimg = "/img/productos/Conjuntos/" + nombreimg;
+                }
+            }
+            if (arrayProductos[i].id_categoria == 7) {
+                const nombreimg = arrayProductos[i].img;
+                if (arrayProductos[i].linkimg == "none") {
+                    arrayProductos[i].linkimg = "/img/productos/Blusas/" + nombreimg;
+                }
+            }
+            if (arrayProductos[i].id_categoria == 8) {
+                const nombreimg = arrayProductos[i].img;
+                if (arrayProductos[i].linkimg == "none") {
+                    arrayProductos[i].linkimg = "/img/productos/Enterizos/" + nombreimg;
+                }
+            }
+            total = total + parseInt(arrayProductos[i].precio);
+            valor = total.toLocaleString("en");
+        }
+        res.render('links/cart', { arrayProductos, bool, valor });
+    } else {
+        res.render('links/cart', { bool });
+    }
 });
 
 module.exports = router;
