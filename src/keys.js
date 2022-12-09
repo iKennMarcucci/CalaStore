0fd5aab2841ab49b4867553e2b1ce7d01c31ebb1
Kenn Marcucci <marcucci5783@gmail.com>
2022-12-09 15:11:05 -0500

    Agregando credenciales de hosteo

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,15 @@
 module.exports = {
     database: {
-        // PARA LOCALHOST
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'calastore'
+        // // PARA LOCALHOST
+        // host: 'localhost',
+        // user: 'root',
+        // password: '',
+        // database: 'calastore'
+
+        // PARA HOSTEO
+        host: 'us-cdbr-east-06.cleardb.net',
+        user: 'b4fefa2abeb128',
+        password: 'f9bb601b',
+        database: 'heroku_5a82b2f600dc2ce'
     }
 }
